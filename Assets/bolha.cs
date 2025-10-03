0dae53b1e2925866e684e489249c2a24a70e5904
Fojojag <joaopedroperezpalumbo@hotmail.com>
2025-10-03 02:57:05 -0300

    ESQUECI DE UM NEGÓCIO

--- Assets/bolha.cs
+++ Assets/bolha.cs
@@ -68,7 +68,7 @@ public class bolha : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {   
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "ground")
         {
             if(isEnemy == true)
         {
